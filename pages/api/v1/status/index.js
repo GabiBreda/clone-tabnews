4dc6876c89b9dec639dcc9b6196c7390b76dc2e7
Gabriel Breda <gabibreda2015@gmail.com>
2026-03-18 20:19:20 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const res = await database.query("SELECT 1 + 1 as sum;");
+  console.log(res.rows);
   response.status(200).json({ chave: "são acima da média" });
-  //.send "não é em UTF8", mas o .json é
 }
 
 export default status;
